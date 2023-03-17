8821a450f409723a81d0d41019a0d791d5791a08
shafay-azeem <shafayazeem12@gmail.com>
2023-03-17 19:41:48 +0500

    first commit

--- src/Components/Main/AboutUs.js
+++ src/Components/Main/AboutUs.js
@@ -16,9 +16,9 @@ export const AboutUs = () => {
             <div className="container py-5" style={{ marginTop: '8rem' }}>
                 <div className="row shadow">
                     <div className="col-md-6 ">
-                        <h1 className='Heading'>About Us</h1>
-                        <h3 className='Heading mb-3'>Sub Heading Goes Here</h3>
-                        <p className='Heading text-center'>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Sapiente voluptatem numquam mollitia necessitatibus quam ipsum ullam, ea laboriosam harum iusto, dolores officia vitae deserunt deleniti. Voluptate facere architecto eius consectetur. Nam, excepturi a repellat odio praesentium perspiciatis soluta reiciendis repudiandae libero, quaerat aperiam dignissimos!x</p>
+                        <h1 className='Heading text-center py-3'>About Us</h1>
+                        <h3 className='sub-Heading mb-3 mt-3 text-center'>Sub Heading Goes Here</h3>
+                        <p className='sub-Heading text-center mt-3'>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Sapiente voluptatem numquam mollitia necessitatibus quam ipsum ullam, ea laboriosam harum iusto, dolores officia vitae deserunt deleniti. Voluptate facere architecto eius consectetur. Nam, excepturi a repellat odio praesentium perspiciatis soluta reiciendis repudiandae libero, quaerat aperiam dignissimos!x</p>
                     </div>
                     <div className="col-md-6">
                         <img className='img-fluid' src={AboutBanner} alt="img" />
